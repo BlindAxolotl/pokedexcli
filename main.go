0d3bbe88194f1d9527c5d7434a537a23bc17d95c
BlindAxolotl <167556385+BlindAxolotl@users.noreply.github.com>
2026-02-20 22:28:06 -0600

    Added input reading to cli

--- main.go
+++ main.go
@@ -10,7 +10,15 @@ func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for {
-		fmt.Print("Pokedex >")
+		fmt.Print("Pokedex > ")
+
+		if scanner.Scan() {
+			fmt.Printf("Testing: %s\n", scanner.Text())
+		}
+
+		if err := scanner.Err(); err != nil {
+			fmt.Println("Error reading input:", err)
+		}
 	}
 }
 
